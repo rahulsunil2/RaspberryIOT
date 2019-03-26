c51bbe5656ed8478e604e69f530e38dbd8442823
rahulsunil2 <rahulsunil2@gmail.com>
2019-03-27 00:55:43 +0530

    LCD using RPLCD Module

---
+++ LCDTest.py
@@ -0,0 +1,11 @@
+#sudo pip install RPLCD
+
+import time
+from RPLCD import CharLCD
+lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
+
+while True:
+    lcd.write_string(u"Hello world!")
+    time.sleep(1)
+    lcd.clear()
+    time.sleep(1)
